71573fb7266bc2c3c5ecad896f6677ef79cd4828
HalAtSent <hcmdavies4@gmail.com>
2026-04-24 14:09:18 +0200

    fix

--- .pi/extensions/pi-orchestrator.js
+++ .pi/extensions/pi-orchestrator.js
@@ -1,6 +1,8 @@
 import { AUTO_BACKEND_MODES } from "../../src/auto-backend-runner.js";
+import { createBuildSessionStore } from "../../src/build-session-store.js";
 import { createPiExtension } from "../../src/pi-extension.js";
 import { createProcessWorkerBackend } from "../../src/process-worker-backend.js";
+import { createRunStore } from "../../src/run-store.js";
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
@@ -8,6 +10,12 @@ const extensionDirectory = dirname(fileURLToPath(import.meta.url));
 const repositoryRoot = resolve(extensionDirectory, "..", "..");
 
 export default createPiExtension({
+  runStore: createRunStore({
+    rootDir: repositoryRoot
+  }),
+  buildSessionStore: createBuildSessionStore({
+    rootDir: repositoryRoot
+  }),
   processWorkerBackend: createProcessWorkerBackend({
     repositoryRoot
   }),
